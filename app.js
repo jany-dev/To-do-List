92f93591a3a6c9862e1653c02c29cbaa09e2d1d7
joya <joyanyanwu14@gmail.com>
2020-08-03 17:00:29 +0100

    remove task list

--- app.js
+++ app.js
@@ -9,35 +9,55 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 // Load all event listeners function
-function loadEventListeners(){
+function loadEventListeners() {
     //Add task event
-form.addEventListener('submit', addTask);
+    form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
 }
-
 // Add Task
-function addTask(e){
-if(taskInput.value === ''){
-    alert(' Add a task ');
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert(' Add a task ');
 
+    }
+
+    // Create li element
+    const li = document.createElement('li');
+    // Add class
+    li.className = 'collection-item';
+    // Create text node and append to the li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // Create new link element
+    const link = document.createElement('a');
+    // Add class
+    link.className = 'delete-item secondary-content';
+    // Add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //Append link to li
+    li.appendChild(link);
+    //Append li to ul
+    taskList.appendChild(li);
+    // Clear input
+    taskInput.value = '';
+    e.preventDefault();
 }
 
-// Create li element
-const li = document.createElement('li');
-// Add class
-li.className = 'collection-item';
-// Create text node and append to the li
-li.appendChild(document.createTextNode(taskInput.value));
-// Create new link element
-const link = document.createElement('a');
-// Add class
-link.className = 'delete-item secondary-content';
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-//Append link to li
-li.appendChild(link);
-//Append li to ul
-taskList.appendChild(li);
-// Clear input
-taskInput.value = '';
-    e.preventDefault();
+// Remove task - target parent element and if statement ot target child element
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    taskList.innerHTML = '';
+    //Faster
+    
+
 }
